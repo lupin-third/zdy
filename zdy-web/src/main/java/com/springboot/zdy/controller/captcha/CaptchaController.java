4a163d97e6dbb58ac4b64e96e1e5da02dcffd288
lupin-third <zzz54751@163.com>
2021-04-15 17:03:56 +0800

    取消注释

--- zdy-web/src/main/java/com/springboot/zdy/controller/captcha/CaptchaController.java
+++ zdy-web/src/main/java/com/springboot/zdy/controller/captcha/CaptchaController.java
@@ -1,9 +1,4 @@
-/*
- *Copyright © 2018 anji-plus
- *安吉加加信息技术有限公司
- *http://www.anji-plus.com
- *All rights reserved.
- */
+
 package com.springboot.zdy.controller.captcha;
 
 import com.springboot.zdy.captcha.common.ResponseModel;

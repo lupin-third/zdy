993ef4c6c6a42bbc17221f87b4a1979b7bfe998d
lupin-third <zzz54751@163.com>
2021-10-09 18:06:30 +0800

    新增logback日志配置文件

--- zdy-web/src/main/java/com/springboot/zdy/ZdyApplication.java
+++ zdy-web/src/main/java/com/springboot/zdy/ZdyApplication.java
@@ -1,14 +1,26 @@
 package com.springboot.zdy;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @MapperScan("com.springboot.zdy.dao")
 @SpringBootApplication
 public class ZdyApplication {
-
+    private static Logger logger = LoggerFactory.getLogger(ZdyApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(ZdyApplication.class, args);
+        String cmd = "C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe http://localhost:8080";
+        Runtime run = Runtime.getRuntime();
+        try {
+            run.exec(cmd);
+            logger.info("启动浏览器打开项目成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
     }
 
 }

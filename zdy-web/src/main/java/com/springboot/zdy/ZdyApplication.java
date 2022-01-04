e10e9f9520a05e66fe8a421a6192e0d3d6213ee8
lupin-third <zzz54751@163.com>
2022-01-04 14:42:31 +0800

    常规更新

--- zdy-web/src/main/java/com/springboot/zdy/ZdyApplication.java
+++ zdy-web/src/main/java/com/springboot/zdy/ZdyApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @MapperScan("com.springboot.zdy.dao")
 @SpringBootApplication
+@EnableEurekaClient
 public class ZdyApplication {
     private static Logger logger = LoggerFactory.getLogger(ZdyApplication.class);
     public static void main(String[] args) {
